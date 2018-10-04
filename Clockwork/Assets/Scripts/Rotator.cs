1d31c7484a882031f77e1841fb596523eb34f5dc
scott sewell <scott.sewell@mail.mcgill.ca>
2018-10-04 18:36:34 -0400

    Added volumetric lighting

---
+++ Clockwork/Assets/Scripts/Rotator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField]
+    private float m_rotationSpeed = 10.0f;
+
+    private void Update()
+    {
+        transform.Rotate(m_rotationSpeed * Time.deltaTime * Vector3.forward, Space.Self);
+    }
+}
