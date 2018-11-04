c9f4a27847b29c235cffac46753f9da20496e0a7
scott sewell <scott.sewell@mail.mcgill.ca>
2018-11-04 01:14:36 -0400

    Added cart graphics and sound effect

---
+++ Clockwork/Assets/Scripts/Cart.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class Cart : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody[] m_wheels;
+
+    [SerializeField]
+    private AudioSource m_wheelSound;
+
+    [SerializeField]
+    [Range(0f, 1f)]
+    private float m_wheelVolumeScale = 0.1f;
+
+    [SerializeField]
+    [Range(0.01f, 1f)]
+    private float m_wheelPitchScale = 0.2f;
+
+    [SerializeField]
+    [Range(0.01f, 1f)]
+    private float m_wheelVolumeSmoothing = 0.2f;
+
+
+    private Rigidbody m_body;
+
+    private void Awake()
+    {
+        m_body = GetComponent<Rigidbody>();
+    }
+    
+    private void Update()
+    {
+        float angVel = 0;
+        foreach (Rigidbody body in m_wheels)
+        {
+            angVel += (m_body.angularVelocity - body.angularVelocity).magnitude;
+        }
+
+        float targetVolume = (m_wheelVolumeScale * angVel) / m_wheels.Length;
+        float targetPitch = (m_wheelPitchScale * angVel) / m_wheels.Length;
+
+        float smoothing = Time.deltaTime / m_wheelVolumeSmoothing;
+        m_wheelSound.volume = Mathf.Lerp(m_wheelSound.volume, targetVolume, smoothing);
+        m_wheelSound.pitch = Mathf.Lerp(m_wheelSound.pitch, targetPitch, smoothing);
+
+        if (m_wheelSound.isPlaying && m_wheelSound.volume < 0.001f)
+        {
+            m_wheelSound.Pause();
+        }
+        else if (!m_wheelSound.isPlaying)
+        {
+            m_wheelSound.Play();
+        }
+    }
+}
